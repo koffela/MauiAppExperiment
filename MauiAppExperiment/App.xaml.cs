72a59fba71307f7d33e459b6aa54ade09fd6cea2
koffela <46707662+koffela@users.noreply.github.com>
2024-07-02 09:09:18 -0700

    initial commit

---
+++ MauiAppExperiment/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppExperiment
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppExperiment/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppExperiment
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
