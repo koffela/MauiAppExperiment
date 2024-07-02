bb874d822db0fe39e6369636f446da717560e144
koffela <46707662+koffela@users.noreply.github.com>
2024-07-02 14:03:21 -0700

    added custom image to .exe

--- MauiAppExperiment/obj/Debug/net8.0-android/MauiAppExperiment.AssemblyInfo.cs
+++ MauiAppExperiment/obj/Debug/net8.0-android/MauiAppExperiment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiAppExperiment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+72a59fba71307f7d33e459b6aa54ade09fd6cea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiAppExperiment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiAppExperiment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
